0255ef902ecef3cd43b467c4b771791f102f06d5
Eric Stiens <estiens@server.fake>
2013-08-27 15:00:13 -0400

    added a database class to hold the teams

--- nokoepl.rb
+++ nokoepl.rb
@@ -17,43 +17,49 @@ goals_away = page.css(".col-ga").map(&:text)
 class Team
   attr_accessor :name, :wins, :losses, :draws, :goals_for, :goals_away, :status
 
-def initialize(name,wins,losses,draws,goals_for,goals_away,status)
-   @name=name
-   @wins=wins.to_i
-   @losses=losses.to_i
-   @draws=draws.to_i
-   @goals_for=goals_for.to_i
-   @goals_away=goals_away.to_i
-   @status=status
+  def initialize(name,wins,losses,draws,goals_for,goals_away,status)
+    @name=name
+    @wins=wins.to_i
+    @losses=losses.to_i
+    @draws=draws.to_i
+    @goals_for=goals_for.to_i
+    @goals_away=goals_away.to_i
+    @status=status
+  end
+
+  def points
+    points=(3*wins)+ draws
+  end
+
+  def goal_difference
+    goal_difference=(goals_for-goals_away)
+  end
+
 end
 
-def points
-   points=(3*wins)+ draws
+class Database
+  attr_accessor :team_array
+
+  def initialize
+    @team_array=[]
+  end
 end
 
-def goal_difference
-  goal_difference=(goals_for-goals_away)
-end
+@db=Database.new
 
-end
-@team=[]
 for x in (0..19)
-  # instancename="team#{x+1}"
-  # puts instancename
-  @team[x]=Team.new(teams[x],wins[x+1],losses[x+1], draw[x+1], goals_for[x+1], goals_away[x+1], status="normal")
-  # puts "#{instancename} is #{instancename.name} with #{instancename.wins} wins #{instancename.losses} losses and #{instancename.draws} draws"
-  # puts "#{instancename.name} has #{instancename.points.to_s} points"
+    @db.team_array<<Team.new(teams[x],wins[x+1],losses[x+1], draw[x+1], goals_for[x+1], goals_away[x+1], status="normal")
 end
 
  
-@team.sort_by! {|x| [-x.points, -x.goal_difference, -x.goals_for]}
-for x in (0..@team.length-1)
-  @team[x].status="champions" if x > -1 && x < 4
-  @team[x].status="relegation" if x > 16
+@db.team_array.sort_by! {|x| [-x.points, -x.goal_difference, -x.goals_for]}
+for x in (0..@db.team_array.length-1)
+  @db.team_array[x].status="champions" if x > -1 && x < 4
+  @db.team_array[x].status="relegation" if x > 16
 end
 
 
-@team.each do |x|
+@db.team_array.each do |x|
    color="blue"
    case x.status
        when "relegation"
@@ -62,7 +68,7 @@ end
          color="green"
    end
 
-  puts "#{x.name} has #{x.points} points and a goal difference of #{x.goal_difference} and a status of #{x.status}".send color
+  puts "#{x.name} has #{x.points} points".send color #and a goal difference of #{x.goal_difference} and a status of #{x.status}".send color
 end
 
 
